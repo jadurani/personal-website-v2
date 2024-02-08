d62556e38f8e76bb0ecc190ec9e12417c4f80921
Jadurani Davalos <jadurani.davalos@gmail.com>
2024-02-08 12:29:43 -0500

    feat: update portfolio page

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -8,38 +8,38 @@ import { usePathname, useRouter } from "next/navigation";
 type ProjectPreview = {
   title: string;
   description: string;
-  thumbnailName: string;
+  thumbnailSrc: string;
   link: string;
 };
 
 const PROJECTS: ProjectPreview[] = [
   {
-    title: "Manage",
+    title: "NOAH, Revamped!",
     description:
-      "This project required me to build a fully responsive landing page to the designs provided. I used HTML5, along with CSS Grid and JavaScript for the areas that required interactivity, such as the testimonial slider.",
-    thumbnailName: "image-portfolio-manage.jpg",
-    link: "manage",
+      "This project required me to design the overall UI/UX and lead the development of the website revamp. My work was a balancing act between creating an interface that was usable for persons comfortable with complexity and those who are not, while at the same time ensuring the technical feasibility of my designs.",
+    thumbnailSrc: "/images/portfolio/noah-revamped/noah-revamped-thumbnail.png",
+    link: "noah-revamped",
   },
   {
-    title: "Bookmark",
+    title: "ReliefAgad",
     description:
-      "This project required me to build a fully responsive landing page to the designs provided. I used HTML5, along with CSS Grid and JavaScript for the areas that required interactivity, such as the features section.",
-    thumbnailName: "image-portfolio-bookmark.jpg",
-    link: "bookmark",
+      "Supporting DSWD's Social Amelioration Program to urgently give much needed-aid to 18 million Filipino families. In this project, I led the development of the self-registration app and built the LGU Registration interface.",
+    thumbnailSrc: "/images/portfolio/reliefagad/reliefagad-thumbnail.png",
+    link: "reliefagad",
   },
   {
-    title: "Insure",
+    title: "360degrees",
     description:
-      "This was a small project which mostly consisted of HTML and CSS. I built a fully-responsive landing page. The only JavaScript this project required was to enable the toggling of the mobile navigation.",
-    thumbnailName: "image-portfolio-insure.jpg",
-    link: "insure",
+      "For this project, I designed and built a fully responsive website for a construction company. Loosely designed via Figma , built with Next.js and Tailwind, and deployed via Vercel.",
+    thumbnailSrc: "/images/portfolio/360degrees/360degrees-thumbnail.png",
+    link: "360degrees",
   },
   {
-    title: "Fylo",
+    title: "FE Mentor - Pomodoro App",
     description:
-      "This project was built in pure HTML and CSS. I had mobile and desktop designs to work to and built it so that it was fully-responsive. I took a mobile-first approach and used modern CSS like Flexbox and Grid for layout purposes.",
-    thumbnailName: "image-portfolio-fylo.jpg",
-    link: "fylo",
+      "A front-end challenge from Frontend Mentor, this pomodoro app provided some tricky challenges, including building a circular progress bar and a range of customizations, like colors and fonts. Built with Next.js and Tailwind, and deployed via Vercel.",
+    thumbnailSrc: "/images/portfolio/fem-pomodoro/fem-pomodoro-thumbnail.png",
+    link: "fem-pomodoro",
   },
 ];
 
@@ -56,21 +56,23 @@ export default function PortfolioPage() {
           <div
             key={idx}
             className={`${idx != 0 ? "mt-16" : ""} px-8 flex flex-col ${idx % 2 ? "md:flex-row-reverse" : "md:flex-row"} gap-4 md:gap-16 justify-center items-stretch`}>
-            <div className="flex-grow w-full relative self-stretch min-h-[320px]">
+            <div className="flex-grow w-full relative self-stretch min-h-[320px] flex items-center">
               <img
                 className="object-cover m-auto"
-                alt={`thumbnail preview for the ${proj.title} project`}
-                srcSet={`/images/portfolio/mobile/${proj.thumbnailName} 320w, /images/portfolio/tablet/${proj.thumbnailName} 768w, /images/portfolio/desktop/${proj.thumbnailName} 1200w`}
+                alt={`thumbnail preview for ${proj.title}`}
+                src={proj.thumbnailSrc}
                 sizes="(max-width: 320px) 280px, (max-width: 768px) 720px, 1200px"
               />
             </div>
 
             <div className="w-full self-stretch flex-grow flex flex-col justify-center py-8 border-t-2 border-b-2 solid border-tuna/5">
-              <div className="font-heading font-bold text-h2 md:text-h1">
+              <div className="font-heading font-bold text-h2 md:text-h1 leading-tight">
                 {proj.title}
               </div>
 
-              <p className="text-tuna/60 py-4">{proj.description}</p>
+              <p className="text-tuna/60 py-4 leading-loose">
+                {proj.description}
+              </p>
 
               <div>
                 <Button
