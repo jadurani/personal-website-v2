9cd6cf1d8cc30979241b201cc23396afdee46808
Jadurani Davalos <jadurani.davalos@gmail.com>
2024-02-08 15:40:59 -0500

    feat: updates on contact form ui

--- src/components/FullNameInput/FullNameInput.tsx
+++ src/components/FullNameInput/FullNameInput.tsx
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
 
 interface FullNameInputProps {
+  disabled?: boolean;
   onBlur: (value: string) => void;
 }
 
-const FullNameInput: React.FC<FullNameInputProps> = ({ onBlur }) => {
+const FullNameInput: React.FC<FullNameInputProps> = ({
+  disabled = false,
+  onBlur,
+}) => {
   const [error, setError] = useState<string | null>(null);
 
   const handleBlur = (value: string) => {
@@ -14,23 +18,26 @@ const FullNameInput: React.FC<FullNameInputProps> = ({ onBlur }) => {
       setError("Only alphanumeric characters and spaces are allowed");
     } else {
       setError(null);
-      onBlur(value)
+      onBlur(value);
     }
   };
 
   return (
     <div className="pb-6 relative font-body">
       <label
-        className={`pl-4 block text-tuna font-bold mb-1 ${ error ? "text-error" : "" }`}>
+        className={`pl-4 block text-tuna font-bold mb-1 ${error ? "text-error" : ""}`}>
         Name
       </label>
       <input
         type="text"
-        className={`appearance-none border-2 w-full py-2 px-3 text-tuna bg-tuna/10 outline-none ${ error ? "border-error" : "focus:border-verdigris"}`}
-        placeholder="Insert your name here..."
+        disabled={disabled}
+        className={`appearance-none border-2 w-full py-2 px-3 text-tuna bg-tuna/10 outline-none disabled:cursor-not-allowed ${error ? "border-error" : "focus:border-verdigris"}`}
+        placeholder="Full Name"
         onBlur={(e) => handleBlur(e.target.value)}
       />
-      <p className="absolute bottom-1 pl-4 text-error font-bold italic text-body2">{error}</p>
+      <p className="absolute bottom-1 pl-4 text-error font-bold italic text-body2">
+        {error}
+      </p>
     </div>
   );
 };
